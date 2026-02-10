a8a68c7481ec76bfdec3d11f83d4ec2572daf40b
A.L. <xenide@users.noreply.github.com>
2026-02-10 14:43:14 -0300

    chore: update hashes

--- script/AngstromL2Factory.s.sol
+++ script/AngstromL2Factory.s.sol
@@ -10,14 +10,14 @@ import {AngstromL2Factory, IHookAddressMiner} from "src/AngstromL2Factory.sol";
 /// @author philogy <https://github.com/philogy>
 contract AngstromL2FactoryScript is BaseScript, Config {
     uint256 constant DEPLOY_TOKEN_ID =
-        0x2508b97b8041960cca8aabc7662f07ec8e285f6d9212c7ea19ea74be25aa4eeb;
-    uint8 constant DEPLOY_TOKEN_NONCE = 127;
+        0x2508b97b8041960cca8aabc7662f07ec8e285f6da1dac8631e4d0aebeb26c77f;
+    uint8 constant DEPLOY_TOKEN_NONCE = 20;
     address constant MULTISIG = 0x2A49fF6D0154506D0e1Eda03655F274126ceF7B6;
 
     // Feb 2027
     uint256 constant GIVE_UP_CLAIM_DEADLINE = 1801752092;
     bytes constant GIVE_UP_CLAIM_SIG =
-        hex"1d25a58942d38130c426f464d49db8397da58827a5913a90c29b4a276ed9afd87eb6fda3ec556da2082c1d64d83dbed8bb1cba6aa74f42e4286a5a5008147e621b";
+        hex"fde82cc31e1ddd10a7139a490d5a7b30a826ba24eb780609eac611b71084a3fe0ac58482dc844490e19e0411577d33cb468133b0a335a3fffb0705f77d577f8e1b";
 
     function run() public {
         _loadConfigAndForks("script/config.toml", false);
@@ -39,9 +39,9 @@ contract AngstromL2FactoryScript is BaseScript, Config {
                 require(address(miner) != address(0), "failed to deploy miner");
             }
 
-            address factory = SUB_ZERO.computeAddress(bytes32(DEPLOY_TOKEN_ID), DEPLOY_TOKEN_NONCE);
-            if (factory.code.length > 0) {
-                console.log("  factory already deployed: %s", factory);
+            AngstromL2Factory factory = AngstromL2Factory(payable(SUB_ZERO.computeAddress(bytes32(DEPLOY_TOKEN_ID), DEPLOY_TOKEN_NONCE)));
+            if (address(factory).code.length > 0) {
+                console.log("  factory already deployed: %s", address(factory));
             } else {
                 bool minted;
                 try SUB_ZERO.getTokenData(DEPLOY_TOKEN_ID) returns (bool _minted, uint8) {
@@ -62,20 +62,24 @@ contract AngstromL2FactoryScript is BaseScript, Config {
                     );
                 }
 
-                factory = SUB_ZERO.deploy(
+                factory = AngstromL2Factory(payable(SUB_ZERO.deploy(
                     DEPLOY_TOKEN_ID,
                     bytes.concat(
-                        type(AngstromL2Factory).creationCode, abi.encode(MULTISIG, uniV4, miner)
+                        type(AngstromL2Factory).creationCode, abi.encode(msg.sender, uniV4, miner)
                     )
-                );
-                console.log("  factory deployed: %s", factory);
+                )));
+                console.log("  factory deployed: %s", address(factory));
             }
 
             require(
                 address(AngstromL2Factory(payable(factory)).UNI_V4()) == uniV4, "uniV4 mismatch"
             );
-            require(AngstromL2Factory(payable(factory)).owner() == MULTISIG, "owner mismatch");
-            require(AngstromL2Factory(payable(factory)).HOOK_ADDRESS_MINER() == miner, "miner mismatch");
+            require(factory.HOOK_ADDRESS_MINER() == miner, "miner mismatch");
+            require(factory.owner() == msg.sender, "owner mismatch");
+
+            factory.setDefaultProtocolSwapFeeMultiple(0.25e6);
+            factory.setDefaultProtocolTaxFee(0.1e6);
+
             vm.stopBroadcast();
         }
     }
